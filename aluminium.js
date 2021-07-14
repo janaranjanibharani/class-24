815514a182a1601ec1829691b66a33a355da9df2
janaranjanibharani <75161006+janaranjanibharani@users.noreply.github.com>
2021-07-14 19:08:53 +0530

    Add files via upload

---
+++ aluminium.js
@@ -0,0 +1,30 @@
+class Aluminium {
+    constructor(x, y) {
+      var options = {
+        'density':4,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 140;
+      this.height = 30;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3.5);
+      stroke('black')
+      fill('RED')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
